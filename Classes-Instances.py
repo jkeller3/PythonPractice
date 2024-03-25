2c3a775fd165d060136b51eda860621f88177107
Joseph Keller <kellerrv@yahoo.com>
2024-03-25 09:22:32 -0700

    Add files via upload

--- Classes-Instances.py
+++ Classes-Instances.py
@@ -20,27 +20,40 @@ class Employee:
 		self.pay = int(self.pay * self.raise_amount)
 
 	@classmethod
-	def set_raise_amt(cls, amount):
-		pass
+	def set_raise_amount(cls, amount):
+		cls.raise_amount = amount
+
+	@classmethod
+	def from_string(cls, emp_str):
+		first, last, pay = emp_str.split('-')
+		return cls(first, last, pay)
+
+	@staticmethod
+	def is_workday(day):
+		if day.weekday() == 5 or day.weekday() == 6:
+			return False
+		return True
+
 
 emp_1 = Employee('Joseph', 'Keller', 60000)
 emp_2 = Employee('New', 'Guy', 50000)
 
-# print(emp_1)
-# print(emp_2)
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
 
-print(emp_1.email)
-print(emp_2.email)
+new_emp_1 = Employee.from_string(emp_str_1)
+new_emp_2 = Employee.from_string(emp_str_2)
+new_emp_3 = Employee.from_string(emp_str_3)
 
-print(emp_1.fullname())
-print(emp_2.fullname())
+print(new_emp_1.email)
+print(new_emp_1.pay)
+print(new_emp_2.email)
+print(new_emp_2.pay)
+print(new_emp_3.email)
+print(new_emp_3.pay)
 
-print(emp_1.userID)
-print(emp_2.userID)
-# emp_1.apply_raise()
-# emp_2.apply_raise()
-# print(emp_1.pay)
-# print(emp_2.pay)
+import datetime
+my_date = datetime.date(2024, 3, 25)
 
-print(emp_1.__dict__)
-print(Employee.__dict__)
+print(Employee.is_workday(my_date))
